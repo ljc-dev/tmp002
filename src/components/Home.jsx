8b4f224166b9034cc0cca97f4ad70130c2941327
ljc-dev <dev.if.ljc@gmail.com>
2021-01-17 12:52:21 +0100

    updated home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
 const Home = () => {
   return (
-    <div className="text-6xl">
+    <div className="text-4xl font-open w-full h-screen flex justify-center items-center">
       Hello World!
     </div>
   )
